f5fbce226f6a0d6e49dc28b2fda28d62d6d97fc6
ZihengZhouuuu <Zhou_Ziheng1@outlook.com>
2024-08-10 14:56:20 +0800

    Content Update & Loading Speed Test

---
+++ js/Lazy.js
@@ -0,0 +1,7 @@
+// main.js - Your JavaScript file
+// Include lazysizes via a CDN
+const script = document.createElement('script');
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js";
+script.async = true;
+document.body.appendChild(script);
+
