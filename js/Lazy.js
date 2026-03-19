8885d44a618e893373606dc8d9bfa35837001d84
ZihengZhouuuu <Zhou_Ziheng1@outlook.com>
2026-03-20 01:38:00 +0800

    Creative Portfolio for Ziheng-V1-WIP

--- js/Lazy.js
+++
@@ -1,7 +0,0 @@
-// main.js - Your JavaScript file
-// Include lazysizes via a CDN
-const script = document.createElement('script');
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js";
-script.async = true;
-document.body.appendChild(script);
-
